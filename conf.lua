169a007241fde6a01f2d34cef017944af2b11401
Mrestof <mrestof@disroot.org>
2024-08-20 23:26:27 +0300

    disable unused modules, add window name

---
+++ conf.lua
@@ -0,0 +1,10 @@
+function love.conf(t)
+  t.window.title = 'Love at Fifteen'
+
+  t.modules.joystick = false
+  t.modules.mouse = false
+  t.modules.touch = false
+  t.modules.physics = false
+  t.modules.system = false
+  t.modules.thread = false
+end
