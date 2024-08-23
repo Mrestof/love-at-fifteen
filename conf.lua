c7207c424bbfe66f214d173679eaaa8bf0c42f1a
Mrestof <mrestof@disroot.org>
2024-08-23 08:09:10 +0300

    disable more unused modules

--- conf.lua
+++ conf.lua
@@ -1,10 +1,12 @@
 function love.conf(t)
   t.window.title = 'Love at Fifteen'
 
+  t.modules.keyboard = false
   t.modules.joystick = false
   t.modules.mouse = false
   t.modules.touch = false
   t.modules.physics = false
   t.modules.system = false
   t.modules.thread = false
+  t.modules.video = false
 end
